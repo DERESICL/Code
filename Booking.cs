8417009e28ef3edac4bb9968fbed4e430848e5e8
Healy,Dermot <dermotjhealy@gmail.com>
2012-06-17 22:25:04 +0100

    test

---
+++ Booking.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Test;
+
+public class Booking
+{
+    int startdate;
+    int enddate;
+    int cost;
+}
---
+++ Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Test;
+
+public class Person
+{
+	int id;
+    string firstname;
+    string lastname;
+    string address1;
+    string address2;
+    string address3;
+    string email;
+    string telephone;
+    
+	
+}
